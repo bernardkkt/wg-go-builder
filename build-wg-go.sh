71fed886f183b0f2dbbdb4e1c243bbd5e8450b98
bernardkkt <kianshunkin@gmail.com>
2021-11-28 13:00:26 +0800

    Update build-wg-go.sh

--- build-wg-go.sh
+++ build-wg-go.sh
@@ -7,12 +7,17 @@ set -e
 # Initialise build environment
 GOPATH=`pwd` go get -v -d golang.zx2c4.com/wireguard
 
+# Define build configurations
+OSARCHCFG="linux:amd64:wireguard-go windows:amd64:wireguard-go.exe darwin:amd64:wireguard-go-darwin linux:arm64:wireguard-go-linux-arm64"
+
 # Build
-OSARCHCFG="linux:amd64:wireguard-go windows:amd64:wireguard-go.exe darwin:amd64:wireguard-go-darwin"
 OLD_PATH=`pwd`
 NEW_PATH=`dirname $(find . -type f -name "go.mod" | grep "wireguard")`
 cd ${NEW_PATH}
 for CFG in ${OSARCHCFG}
 do
-	GOPATH=${OLD_PATH} GOOS=`echo ${CFG} | cut -d ':' -f 1` GOARCH=`echo ${CFG} | cut -d ':' -f 2` go build -v -o "${OLD_PATH}/`echo ${CFG} | cut -d ':' -f 3`"
+	GOOS=`echo ${CFG} | cut -d ':' -f 1`
+	GOARCH=`echo ${CFG} | cut -d ':' -f 2`
+	GOPATH=${OLD_PATH}
+	go build -v -o "${OLD_PATH}/`echo ${CFG} | cut -d ':' -f 3`"
 done
