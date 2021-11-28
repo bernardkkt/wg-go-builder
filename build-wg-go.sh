ef239bd9a979a1da688a4548051434b8c510f04f
bernardkkt <kianshunkin@gmail.com>
2021-11-28 15:15:08 +0800

    Create more builds for different platform (#5)
    Build binaries for Windows (amd64),  MacOS (amd64) and Linux (arm64) in addition to Linux (amd64)

--- build-wg-go.sh
+++ build-wg-go.sh
@@ -7,8 +7,14 @@ set -e
 # Initialise build environment
 GOPATH=`pwd` go get -v -d golang.zx2c4.com/wireguard
 
+# Define build configurations
+OSARCHCFG="linux:amd64:wireguard-go windows:amd64:wireguard-go.exe darwin:amd64:wireguard-go-darwin linux:arm64:wireguard-go-linux-arm64"
+
 # Build
 OLD_PATH=`pwd`
 NEW_PATH=`dirname $(find . -type f -name "go.mod" | grep "wireguard")`
 cd ${NEW_PATH}
-GOPATH=${OLD_PATH} GOOS=linux GOARCH=amd64 go build -v -o "${OLD_PATH}/wireguard-go"
+for CFG in ${OSARCHCFG}
+do
+	GOOS=`echo ${CFG} | cut -d ':' -f 1` GOARCH=`echo ${CFG} | cut -d ':' -f 2` GOPATH=${OLD_PATH} go build -v -o "${OLD_PATH}/`echo ${CFG} | cut -d ':' -f 3`"
+done
